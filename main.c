9576b1e752f820fca0980a0728ef4007a4f3ee96
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-08-10 17:45:18 +0100

    feat: variables

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    // VARIABLES
+
+    // double pi = 3.141592;
+
+    // printf("The value of pi is %lf",pi);
+
+    // float gpa = 2.5;
+    // float price = 19.99;
+    // float temperature = -10.1;
+
+    // printf("Your gpa is %f\n", gpa);
+    // printf("The price is $%f\n",price);
+    // printf("The temperature is %.f degree\n",temperature);
+
+    // int age = 10;
+    // int year = 2025;
+    // int quantity = 1;
+
+    // printf("You are %d years old\n", age);
+    // printf("The year is %d\n", year);
+    // printf("You have ordered %d x items\n",quantity);
+
+    // my first c program! :)
+    // printf("Hello c++\n");
+    // printf("It's really good");
+
+    // char name[] = "Daniel";
+    // char food[] = "Pizza";
+
+    // // char name = 'x';
+
+    // printf("Hello %s\n", name);
+    // printf("Your favorite food is %s\n", food);
+
+    // bool isOnline = 0;
+
+    // // printf("This user is %d", isOnline);
+
+    // if (isOnline)
+    // {
+    //     printf("This user is online");
+    // }
+    // else
+    // {
+    //     printf("This user is offline");
+    // }
+
+    // FORMAT SPECIFIERS
+
+    return 0;
+}
