d17fd46356d3b418e2bcc3bc30f45609b9d89030
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-08-11 12:26:12 +0100

    MAD LIBS GAME

--- main.c
+++ main.c
@@ -126,26 +126,65 @@ int main()
 
     // SHOPPING CART PROGRAM
 
-    char item[50] = "";
-    float price = 0.0f;
-    int quantity = 0;
-    char currency = '$';
-    float total = 0.0f;
+    // char item[50] = "";
+    // float price = 0.0f;
+    // int quantity = 0;
+    // char currency = '$';
+    // float total = 0.0f;
 
-    printf("What item would you like to buy?: ");
-    fgets(item, sizeof(item), stdin);
-    item[strlen(item) - 1] = '\0';
+    // printf("What item would you like to buy?: ");
+    // fgets(item, sizeof(item), stdin);
+    // item[strlen(item) - 1] = '\0';
 
-    printf("What is the price for each? ");
-    scanf("%f", &price);
+    // printf("What is the price for each? ");
+    // scanf("%f", &price);
 
-    printf("how many would you like? ");
-    scanf("%d", &quantity);
+    // printf("how many would you like? ");
+    // scanf("%d", &quantity);
 
-    total = price * quantity;
+    // total = price * quantity;
 
-    printf("\nYou have bought %d %s\n", quantity, item);
-    printf("%c%.2f\n", currency, total);
+    // printf("\nYou have bought %d %s\n", quantity, item);
+    // printf("%c%.2f\n", currency, total);
+
+    // MAD LIBS GAME
+
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0';
+
+    printf("Enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
+
+    printf("Enter a verb (ending w/ -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
+
+    // printf("%s\n", noun);
+    // printf("%s\n", verb);
+    // printf("%s\n", adjective1);
+    // printf("%s\n", adjective2);
+    // printf("%s\n", adjective3);
+
+    printf("\nToday I went to a %s zoo.\n", adjective1);
+    printf("In an exhibit, i saw a %s\n", noun);
+    printf("%s was %s and %s!\n", noun, adjective2, verb);
+    printf("I was %s!\n", adjective3);
 
     return 0;
 }
