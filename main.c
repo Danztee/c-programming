38e04d1904e4aac705f036247aeda7bfb1f724f2
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-08-11 08:44:33 +0100

    shopping cart

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main()
 {
-
     // VARIABLES
 
     // double pi = 3.141592;
@@ -84,6 +84,68 @@ int main()
     // printf("%.1f\n", price1);
     // printf("%.2f\n", price2);
     // printf("%.2f\n", price3);
+
+    // int x = 10;
+    // int y = 2;
+    // int z = 0;
+
+    // // z = x + y;
+    // // z = x - y;
+    // // z = x * y;
+
+    // // z = x / y;
+
+    // x = x + 2;
+
+    // printf("%d", x);
+
+    // printf("Hello");
+
+    // int age = 0;
+    // float gpa = 0.0f;
+    // char grade = '\0';
+    // char name[30] = "";
+
+    // printf("Enter your age? ");
+    // scanf("%d", &age);
+
+    // printf("Enter your GPA? ");
+    // scanf("%f", &gpa);
+
+    // printf("Enter your grade? ");
+    // scanf(" %c", &grade);
+
+    // getchar();
+    // printf("Enter your first name");
+    // fgets(name, sizeof(name), stdin);
+    // name[strlen(name) - 1] = '\0';
+
+    // printf("%d\n", age);
+    // printf("%.2f\n", gpa);
+    // printf("%c\n", grade);
+
+    // SHOPPING CART PROGRAM
+
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
+
+    printf("What is the price for each? ");
+    scanf("%f", &price);
+
+    printf("how many would you like? ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("\n You have bought %d %s\n", quantity, item);
+    printf("%c%.2f\n", currency, total);
 
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+    // printf("What item would you like to buy?: ");
+    // fgets(items, sizeof(items), stdin);
